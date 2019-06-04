9c63f75b2136d72ef88fe44c7645e711321827af
Phil Brookes <phil.brookes@gmail.com>
2019-06-04 17:16:24 +0100

    Updates

---
+++ precache-manifest.1b7c19a4e68b89b4ace707fff3962210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fbfe729e13d1db401fe7d2c50483de0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9c390e24abfedb863c3",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "8084f9bd8edf81b6ecd2",
+    "url": "/static/css/main.9c29dbde.chunk.css"
+  },
+  {
+    "revision": "c9c390e24abfedb863c3",
+    "url": "/static/js/2.b8753306.chunk.js"
+  },
+  {
+    "revision": "8084f9bd8edf81b6ecd2",
+    "url": "/static/js/main.b5b63c3f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/glasto-app/precache-manifest.f3530d225e36a3eae64dae625dc449e4.js"
+  "/precache-manifest.1b7c19a4e68b89b4ace707fff3962210.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/glasto-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
